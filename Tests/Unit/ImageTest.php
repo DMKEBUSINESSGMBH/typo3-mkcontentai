aacfb129bc0a983c2ede2c0a490f93fd004f4a98
Hannes Bochmann <hannes.bochmann@dmk-ebusiness.com>
2023-05-22 13:23:12 +0200

    Cleanup fix copyright hint

--- Tests/Unit/ImageTest.php
+++ Tests/Unit/ImageTest.php
@@ -8,7 +8,7 @@ declare(strict_types=1);
  * (c) DMK E-BUSINESS GmbH <dev@dmk-ebusiness.de>
  * All rights reserved
  *
- * This file is part of TYPO3 CMS-based extension "container" by b13.
+ * This file is part of TYPO3 CMS-based extension "mkcontentai" by DMK E-BUSINESS GmbH.
  *
  * It is free software; you can redistribute it and/or modify it under
  * the terms of the GNU General Public License, either version 2
@@ -18,7 +18,7 @@ declare(strict_types=1);
 namespace DMK\MkContentAi\Tests\Unit;
 
 /*
- * This file is part of TYPO3 CMS-based extension "container" by b13.
+ * This file is part of TYPO3 CMS-based extension "mkcontentai" by DMK E-BUSINESS GmbH.
  *
  * It is free software; you can redistribute it and/or modify it under
  * the terms of the GNU General Public License, either version 2
