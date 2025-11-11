78915763e5a2ca629cc63d98d7cc9d260ebd4485
Hannes Bochmann <hannes.bochmann@dmk-ebusiness.com>
2025-11-11 11:20:51 +0100

    Cleanup add copyright

--- Classes/Utility/SettingsUtility.php
+++ Classes/Utility/SettingsUtility.php
@@ -1,5 +1,18 @@
 <?php
 
+/*
+ * Copyright notice
+ *
+ * (c) DMK E-BUSINESS GmbH <dev@dmk-ebusiness.de>
+ * All rights reserved
+ *
+ * This file is part of TYPO3 CMS-based extension "mkcontentai" by DMK E-BUSINESS GmbH.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ */
+
 namespace DMK\MkContentAi\Utility;
 
 use DMK\MkContentAi\Controller\AiImageController;
@@ -20,6 +33,7 @@ class SettingsUtility
     public function isApiKeySetForImageGeneration(): bool
     {
         $imageAiEngine = SettingsController::getImageAiEngine();
+
         return $this->isApiKeySetForClient(AiImageController::GENERATOR_ENGINE[$imageAiEngine]);
     }
 
